0d784b54fa1e58de1dbc8ba4b2cce754a7d8e8b0
kogarasi217 <aizu.188@gmail.com>
2023-12-21 20:27:21 +0900

    [add] remove top page streams

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,7 @@
 import remoteChannelPlayerDisclosures from "./func/remove-channel-player-disclosures";
+import removeTopPageStreams from "./func/remove-top-page-streams";
 
 window.onload = () => {
   remoteChannelPlayerDisclosures();
+  removeTopPageStreams();
 };
